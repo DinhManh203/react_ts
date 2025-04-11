16ce92f273dec0c8184a63084d22ba605c060bf3
DinhManh203 <dinhmanhoke203@gmail.com>
2025-04-11 11:59:02 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Layout from './layout'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './pages/client/auth/login';
+import RegisterPage from './pages/client/auth/register';
+import './styles/global.scss';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Layout />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/client/book.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookPage = () => {
+  return (
+    <div>book</div>
+  )
+}
+
+export default BookPage
